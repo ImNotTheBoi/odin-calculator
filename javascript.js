0d414bd3770d018e29451129a5f3856c58cb5638
ImNotTheBoi <174925899+ImNotTheBoi@users.noreply.github.com>
2024-08-05 22:33:24 +0800

    Calculations
    Able to calculate with multiple operations.
    It does'nt evaluate more than a single pair of numbers at a time.
    Fix pressing "=" before writing all the numbers.
    Fix breaking the calculator when dividing by 0

--- javascript.js
+++ javascript.js
@@ -26,30 +26,82 @@ function operate() {
     let result = 0
     switch (operation) {
         case "+":
-            result = add(number1, number2);
+            result = add(+number1, +number2);
             break;
         case "-":
-            result = subtract(number1, number2);
+            result = subtract(+number1, +number2);
             break;
-        case "*":
-            result = multiply(number1, number2);
+        case "x":
+            result = multiply(+number1, +number2);
             break;
         case "/":
-           result = divide(number1, number2);
+            if (+number2 === 0) {
+                console.log("kys")
+                return "ERROR"
+            }
+            result = divide(+number1, +number2);
             break;
     };
-    return result
+    number1 = `${result}`
+    number2 = "";
+    console.log(`${result}`)
+    return `${result}`
 };
 
 function displayText(string, type) {
     switch (type) {
         case "number":
-            number1 += string
-            display = number1
-            console.log(string)
-            console.log(number1)
+            if (operation === "") {
+                number1 += string
+                console.log(number1)
+            }
+            else {
+                number2 += string
+                console.log(number2)
+            }
+            display += string
             break;
         case "operator":
+            if (operation === "") {
+                operation = string
+                display += ` ${string} ` 
+            }
+            else {
+                operate()
+                operation = string
+                display += ` ${string} `
+            }
+            break;
+        case "equal":
+            if (number1 && number2 && operation) {
+                let result = operate()
+                if (result === "ERROR") {
+                    number1 = "";
+                    number2 = "";
+                    operation = "";
+                    display = ""
+                }
+                else {
+                    operation = "";
+                    display = result
+                }
+                break;
+            }
+            else {
+                console.log("ERROR")
+                console.log(number1)
+                console.log(number2)
+                console.log(operation)
+                number1 = "";
+                number2 = "";
+                operation = "";
+                display = ""
+            }
+        case "clear":
+            number1 = "";
+            number2 = "";
+            operation = "";
+            display = ""
             break;
     }
     displayElement.textContent = display
@@ -57,14 +109,25 @@ function displayText(string, type) {
 
 function buttonFunctions(buttons) {
     for (let btn of buttons) {
-        console.log(btn)
         let className = btn.className;
+        let keyType = ""
         switch (className) {
             case "keys number":
-                btn.addEventListener("click", () => {
-                    displayText(btn.textContent, "number")
-                })
+                keyType = "number"
+                break;
+            case "keys operator":
+                keyType = "operator"
+                break;
+            case "keys equal":
+                keyType = "equal"
+                break;
+            case "keys clear":
+                keyType = "clear"
+                break;
         }
+        btn.addEventListener("click", () => {
+            displayText(btn.textContent, `${keyType}`)
+        })
     }
 }
 buttonFunctions(keys)
